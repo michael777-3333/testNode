862b296406214ba1511efd9bccfd5ba89bfaf61f
wildmer <wilmerdiazolaya@gmail.com>
2024-11-11 03:33:37 -0500

    feat: add dinamic route import

--- models/server.js
+++ models/server.js
@@ -1,42 +1,53 @@
 import express from 'express';
 import fs from 'fs'
 import path from 'path';
+import {pathToFileURL} from 'url';
 
 class Server {
-    constructor() {
-        this.app = express();
-    }
+  constructor() {
+    this.app = express();
+  }
 
-    async routes(dir) {
-        if (dir) {
-            const files = fs.readdirSync(dir);
-    
-        for (const file of files) {
-          const filePath = path.join(dir, file);
-    
-          if (fs.lstatSync(filePath).isFile() && file.endsWith('.js')) {
-            const routeName = `/${file.replace('.js', '')}`;
-            
-            const routeModule = await import(filePath);
-    
-            if (routeModule.default) {
-              this.app.use(routeName, routeModule.default); 
-              console.log(`Rutas registradas: ${routeName}`);
-            } else {
-              console.warn(`Módulo sin exportación predeterminada en ${filePath}`);
-            }
+  async routes(dir) {
+    console.log('Registrando rutas...');
+    if (dir) {
+      const files = fs.readdirSync(dir);
+      for (const file of files) {
+        const filePath = path.join(dir, file);
+        // path to file URL
+        const modelURL = pathToFileURL(filePath).href;
+
+        if (fs.lstatSync(filePath).isFile() && file.endsWith('.js')) {
+          const routeName = `/${file.replace('.js', '')}`;
+
+          const routeModule = await import(modelURL);
+
+          if (routeModule.default) {
+            this.app.use(routeName, routeModule.default);
+            console.log(`Rutas registradas: ${routeName}`);
+          } else {
+            console.warn(`Módulo sin exportación predeterminada en ${filePath}`);
           }
         }
-        }
-        
+      }
     }
 
+  }
 
-    listen() {
-        this.app.listen(3000, () => { console.log('Server Online: ', 3000); });
-    }
+
+  listen() {
+    this.app.listen(3000, () => { console.log('Server Online: ', 3000); });
+  }
 
 
 }
 
 export default Server;
+
+// tests: C:\Users\wilme\Documents\python\n\testNode\routes\pets
+// Server Online:  3000
+// node:internal/errors:496
+//     ErrorCaptureStackTrace(err);
+//     ^
+
+// Error [ERR_UNSUPPORTED_ESM_URL_SCHEME]: Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. On Windows, absolute paths must be valid file:// URLs. Received protocol 'c:'
